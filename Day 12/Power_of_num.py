3dcb1a77cb06ef663cfc2d3b28646d3194b18454
sg-kiran <kiranreddymq@gmail.com>
2025-11-25 14:00:24 +0100

    Add files via upload

---
+++ Day 12/Power_of_num.py
@@ -0,0 +1,19 @@
+"""
+a = int((input('enter the value')))
+b = int((input('enter the power value')))
+
+c = int(a ** b)
+print(c)
+"""
+"""
+
+# Example: 2 raised to the power of 3
+result = 2 ** 3
+print(result)  # Output: 8
+
+"""
+
+def square(num:int, num1:int) -> float:
+    a=num ** num1
+    return a
+print("result :", square(2,3))
