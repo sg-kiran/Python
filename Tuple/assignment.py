80462310b9f939863aa298d99964e9812472ed38
sg-kiran <kiranreddymq@gmail.com>
2025-12-12 14:25:15 +0100

    tuple assignment e2e

--- Tuple/assignment.py
+++ Tuple/assignment.py
@@ -19,7 +19,59 @@ print(type(t5))
 t6 = 12,13,14
 """8. What is tuple unpacking? Write an example program."""
 t7 = 1,2,3,4,5,6,7,8,9
-a,b,*c=t7
+a,*b, c=t7
 print(a)
 print(b)
 print(c)
+"""9. What happens if the number of variables on the left side does not match the number of values in a tuple during unpacking?"""
+# t8=3,4,5
+# d,e,f,g=t8
+# print(d, e, f, g)
+#Not enough values to unpack(expected 4, got 3)
+"""11. Can tuples contain mutable elements? Explain with an example."""
+"""15. Can we modify elements inside a tuple? If not, how can we indirectly modify the content?"""
+# Yes, tuples can contain mutable elements like lists
+t9 = (2,3,4,[5,6])
+print(t9)
+t9[3].append(7)
+print(t9)
+"""12. Are tuples immutable? What does immutability mean in this context?"""
+"""
+Yes, tuples are immutable, which means the structure of the tuple cannot be changed but if the 
+element itself is mutable then that can be changed.
+"""
+
+"""13. How can you access elements inside a tuple?"""
+"""
+1. using indexing
+2.slicing
+3. using loops
+"""
+#t9 = (2,3,4,[5,6])
+print("using indexing",t9[1])
+print("using slicing",t9[0:4])
+for i in t9: #printing using for loop and if the element is a value, printing those values also individually
+    if(type(i)==list):
+        print("below are the values from list")
+        for j in i:
+            print(j,"is of type", type(j))
+    else:
+        print(i,"is of type", type(i))
+
+
+"""14. What is nested tuple unpacking? Provide a scenario where it is used."""
+"""
+nested tuple unpacking is used to unpack the tuple elements of type list of tuple """
+
+print("nested tuple unpacking", t9[3][1])
+"""16. How does tuple assignment help in swapping values?"""
+a,b=(2,3)
+(b,a)=(a,b)
+print(a)
+print(b)
+
+"""19. Can two tuples be concatenated? How?"""
+t11= t6+t7
+print(t11)
+# t12= t6-t7
+# print(t12) this will not work as - is not supported for tuple
