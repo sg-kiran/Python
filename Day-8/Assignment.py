748c7ac0e25c046987dfa6c0d8611e8c30afe404
sg-kiran <kiranreddymq@gmail.com>
2026-01-16 12:41:12 +0100

    day 8 assignment

---
+++ Day-8/Assignment.py
@@ -0,0 +1,17 @@
+"""
+Q. What is reassignment? How do you do this.
+A. reassignment means overriding the existing value of a variable and giving it a new value
+a=2
+print(a)
+a=3
+print(a)
+Python reassigns the name to a new object (rebinding), rather than “overriding the value.”
+"""
+
+"""
+Q. What do you mean by `Python is a dynamic typing language`. Can you please give a brief explaination with some examples. 
+A. It means there is no need define the data type along with the variable and python will automatically identifies which data type you have entered.
+examples:
+a =3 # it is of data type int
+name = "Kiran" # it is of data type str
+"""
--- Patterns/assignment.py
+++ Patterns/assignment.py
@@ -1,4 +1,40 @@
-for i in range(6):
-    print('* '* 6)
 
+# #square fill pattern
+# for i in range(6):
+#     print('* '* 6)
+# print("============================================================")
 
+# #Right half pyramid
+#
+# for i in range(1, 6):
+#     print('* ' *i)
+# print("============================================================")
+
+# #Reverse Right half pyramid
+#
+# for i in range(5, 0, -1):
+#     print('* ' * i)
+# print("============================================================")
+
+# #Left half pyramid
+n = 5
+for i in range(1,n+1):
+    spaces = "  "* (n-i)
+    stars = " *"* i
+    print(spaces,stars)
+print("============================================================")
+
+# # reverse Left half pyramid
+# # n = 5
+# # for i in range(n,0,-1):
+# #     spaces = "  "* (n-i)
+# #     stars = " *"* i
+# #     print(spaces + stars)
+#
+# m =5
+# for i in range(m+1,0,-1):
+#     spaces1 = " "*(m-(i-1))
+#     stars1 = " *"*(6-i)
+#     print(spaces1+stars1)
+#
+#
