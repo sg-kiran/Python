18aaa83ec54d516e10b9c9885bb3a8257d86582b
sg-kiran <kiranreddymq@gmail.com>
2026-02-08 20:08:43 +0530

    staticmethod

---
+++ class_objects/Day-31/staticmethod.py
@@ -0,0 +1,20 @@
+
+class Student:
+    SchoolName = "Balayesu"
+    def __init__(self, name:str, age:int, marks:str):
+        self.name = name
+        self.age = age
+        self.marks = Student.str_to_int(marks)
+
+
+
+    @classmethod
+    def modifyschoolname(cls, name:str):
+        cls.SchoolName = name
+
+    @staticmethod
+    def str_to_int(marks:str):
+        return int(marks)
+
+S1 = Student("Kiran",22, "234")
+print(S1.marks)
