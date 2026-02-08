717cd3c1c116322ee69bb83a611934bd6e3f1b53
sg-kiran <kiranreddymq@gmail.com>
2026-02-08 16:36:14 +0530

    class objects basics

--- class_objects/Day-29/assignment.py
+++ class_objects/Day-29/assignment.py
@@ -17,14 +17,34 @@ class mobile:
         self.model=model
         self.Year=Year
     def music(self, company:str):
+        self.company=company
         print(f"playing music on {self.color} and {self.model} {self.company}phone")
+
+    def call(self):  # Method
+         return "Calling"
+
+    def music_player(self):
+        print("Playing Music.")
+
+    def video_player(self):
+        print("Playing a Movie.")
 
 nokiamobile = mobile("red","Nokia")
 print(nokiamobile.Year)
-print(nokiamobile.music("India"))
+print(nokiamobile.call())
 
-# class tv:
-#     def __init__(self):
-#         self.model = 2023
-# lg_tv=tv()
-# print(lg_tv.model)
+
+# class car:
+#     def __init__(self,colour:str, price:float, made:int):
+#         self.colour = colour
+#         self.price = price
+#         self.made = made
+#     def music(self):
+#         print(f"listening music in {self.colour} car ")
+#
+# white_car = car("white", 20000, 2023)
+# print(white_car.colour)
+# print(white_car.price)
+# print(white_car.music())
+#
+#
