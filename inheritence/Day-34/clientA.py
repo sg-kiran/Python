df97c2af7ec9c16b710e2baa71c700bcaa48ed02
sg-kiran <kiranreddymq@gmail.com>
2026-02-08 20:52:35 +0530

    inheritence basics

---
+++ inheritence/Day-34/clientA.py
@@ -0,0 +1,7 @@
+from maths_functions import math, multiplication
+
+A= math()
+B= multiplication()
+print(A.addition(10,12))
+
+print(B.mul(10,12))
---
+++ inheritence/Day-34/maths_functions.py
@@ -0,0 +1,13 @@
+class math:
+
+    def addition(self, a,b):
+        return a+b
+    def subtraction(self, a,b):
+        return a-b
+
+class multiplication:
+
+    def mul(self, a,b):
+        return a*b
+
+
